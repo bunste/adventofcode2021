1f1893865607e4424f806010e9fa1d44f3fd4282
Stefan Bungter <stefan.bungter@pressrelations.de>
2022-03-22 18:27:17 +0100

    Refactored code to also solve day 1 part 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,5 +1,18 @@
 defmodule AdventOfCode.Day1 do
-  def count([a, b | tail], acc) when b > a, do: count([b | tail], acc+1)
-  def count([a, b | tail], acc), do: count([b | tail], acc)
-  def count(_, acc), do: acc
+  def increases(list, window_size \\ 1) do
+    list
+    |> preprocess(window_size)
+    |> count(0)
+  end
+
+  defp preprocess(list, 1), do: list
+  defp preprocess(list, window_size) do
+    list
+    |> Enum.chunk_every(window_size, 1, :discard)
+    |> Enum.map(&Enum.sum(&1))
+  end
+
+  defp count([a, b | tail], acc) when b > a, do: count([b | tail], acc+1)
+  defp count([_a, b | tail], acc), do: count([b | tail], acc)
+  defp count(_, acc), do: acc
 end
