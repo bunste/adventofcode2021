5097cf3207e1df4d1810bdd7fa90fc3b33f0a81e
Stefan Bungter <stefan.bungter@pressrelations.de>
2022-04-15 20:56:17 +0200

    Added solution for day 3 part 2

--- lib/day3.ex
+++ lib/day3.ex
@@ -12,6 +12,7 @@ defmodule AdventOfCode.Day3 do
       diagnostic_report
       |> Enum.map(&String.graphemes(&1))
       |> swap_rows_with_columns()
+      |> Enum.reverse()
       |> Enum.reduce(acc, fn column, acc ->
         new_gamma_rate = [most_common(column) | acc.gamma_rate]
         new_epsilon_rate = [least_common(column) | acc.epsilon_rate]
@@ -25,16 +26,27 @@ defmodule AdventOfCode.Day3 do
   defp swap_rows_with_columns(diagnostic_report) do
     diagnostic_report
     |> Enum.zip_reduce([], fn elements, acc -> [elements | acc] end)
+    |> Enum.reverse()
   end
 
   defp most_common(elements) do
-    f = Enum.frequencies(elements)
-    if f["0"] > f["1"], do: 0, else: 1
+    f =
+      elements
+      |> Enum.frequencies()
+      |> Map.put_new("0", 0) # In case we don't have any 0s
+      |> Map.put_new("1", 0) # In case we don't have any 1s
+
+    if f["0"] > f["1"], do: "0", else: "1"
   end
 
   defp least_common(elements) do
-    f = Enum.frequencies(elements)
-    if f["0"] < f["1"], do: 0, else: 1
+    f =
+      elements
+      |> Enum.frequencies()
+      |> Map.put_new("0", 0) # In case we don't have any 0s
+      |> Map.put_new("1", 0) # In case we don't have any 1s
+
+    if f["0"] > f["1"], do: "1", else: "0"
   end
 
   defp to_decimal(list) do
@@ -43,4 +55,31 @@ defmodule AdventOfCode.Day3 do
     |> Integer.parse(2)
     |> elem(0)
   end
+
+  # Part 2
+
+  def life_support_rating(diagnostic_report) do
+    rows =
+      diagnostic_report
+      |> Enum.map(&String.graphemes(&1))
+
+    oxygen_generator_rating = compute(rows, &most_common/1, 0)
+    co2_scrubber_rating = compute(rows, &least_common/1, 0)
+
+    to_decimal(oxygen_generator_rating) * to_decimal(co2_scrubber_rating)
+  end
+
+  def compute([row], _, _), do: row
+  def compute(rows, criteria, position) do
+    filter_value =
+      rows
+      |> swap_rows_with_columns()
+      |> Enum.at(position)
+      |> criteria.()
+
+    rows
+    |> Enum.filter(fn row -> Enum.at(row, position) == filter_value end)
+    |> compute(criteria, position+1)
+  end
+
 end
