dabfb856bdcb122449d3953a066fcd661f095463
Stefan Bungter <stefan.bungter@pressrelations.de>
2022-04-15 21:10:25 +0200

    Refactored solution for day 3

--- lib/day3.ex
+++ lib/day3.ex
@@ -14,8 +14,10 @@ defmodule AdventOfCode.Day3 do
       |> swap_rows_with_columns()
       |> Enum.reverse()
       |> Enum.reduce(acc, fn column, acc ->
-        new_gamma_rate = [most_common(column) | acc.gamma_rate]
-        new_epsilon_rate = [least_common(column) | acc.epsilon_rate]
+        {most_common, least_common} = most_and_least_common(column)
+
+        new_gamma_rate = [most_common | acc.gamma_rate]
+        new_epsilon_rate = [least_common | acc.epsilon_rate]
 
         %{ acc | gamma_rate: new_gamma_rate, epsilon_rate: new_epsilon_rate }
       end)
@@ -29,25 +31,20 @@ defmodule AdventOfCode.Day3 do
     |> Enum.reverse()
   end
 
-  defp most_common(elements) do
+  # Returns a tuple {most_common_element, least_common_element}
+  def most_and_least_common(elements) do
     f =
       elements
       |> Enum.frequencies()
       |> Map.put_new("0", 0) # In case we don't have any 0s
       |> Map.put_new("1", 0) # In case we don't have any 1s
 
-    if f["0"] > f["1"], do: "0", else: "1"
+    if f["0"] > f["1"], do: {"0", "1"}, else: {"1", "0"}
   end
 
-  defp least_common(elements) do
-    f =
-      elements
-      |> Enum.frequencies()
-      |> Map.put_new("0", 0) # In case we don't have any 0s
-      |> Map.put_new("1", 0) # In case we don't have any 1s
+  defp most_common({most_common, _}), do: most_common
 
-    if f["0"] > f["1"], do: "1", else: "0"
-  end
+  defp least_common({_, least_common}), do: least_common
 
   defp to_decimal(list) do
     list
@@ -70,16 +67,17 @@ defmodule AdventOfCode.Day3 do
   end
 
   def compute([row], _, _), do: row
-  def compute(rows, criteria, position) do
+  def compute(rows, bit_criteria, position) do
     filter_value =
       rows
       |> swap_rows_with_columns()
       |> Enum.at(position)
-      |> criteria.()
+      |> most_and_least_common()
+      |> bit_criteria.()
 
     rows
     |> Enum.filter(fn row -> Enum.at(row, position) == filter_value end)
-    |> compute(criteria, position+1)
+    |> compute(bit_criteria, position+1)
   end
 
 end
